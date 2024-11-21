d0d3e2488cfdd0743fc02c5c9537647f5431c4be
user.email <0kq@0kqnet.work>
2024-11-21 13:03:00 +0900

    update assets

--- test/test.py
+++ test/test.py
@@ -3,9 +3,10 @@ sys.path.append('../')
 
 from enkanetwork import EnkaNetworkAPI, Language
 import asyncio
+import logging
 
 async def main():
-    client = EnkaNetworkAPI(lang=Language.JP)
+    client = EnkaNetworkAPI(lang=Language.JP,debug=True)
     resp = await client.fetch_user_by_uid(800857830)
     print(resp.player.tower_act)
     print("fetch user OK")
